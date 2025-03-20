c4d1f41255ce04a10f4a82b1cc639038262ec5a0
Yosefrgb <yocabamo@gmail.com>
2025-03-20 09:48:38 -0500

    version1

---
+++ main.py
@@ -0,0 +1,17 @@
+# main.py -- put your code here!
+from machine import Pin, ADC
+from time import sleep
+from machine import mem32
+ 
+peatonal1verde=Pin(23,Pin.OUT)
+peatonal2verde=Pin(22,Pin.OUT)
+peatonal3verde=Pin(21,Pin.OUT)
+vehicular4amarillo=Pin(19,Pin.OUT)
+vehicular5verde=Pin(18,Pin.OUT)
+vehicular6verde=Pin(5,Pin.OUT)
+peatonal1rojo=Pin(13,Pin.OUT)
+peatonal2rojo=Pin(12,Pin.OUT)
+peatonal3rojo=Pin(14,Pin.OUT)
+vehicular4rojo=Pin(27,Pin.OUT)
+vehicular5rojo=Pin(26,Pin.OUT)
+vehicular6rojo=Pin(25,Pin.OUT)
